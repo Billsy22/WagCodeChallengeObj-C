e1dcfbe4720e2c6b458fbf341fee170bab5a9f5c
Billsy22 <35415537+Billsy22@users.noreply.github.com>
2018-06-05 17:28:36 -0600

    Began making custom cell and finished initial setup of storyboard

---
+++ WagCodeChallengeObj-C/View Controller/SinglePostTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  SinglePostTableViewCell.h
+//  WagCodeChallengeObj-C
+//
+//  Created by Taylor Bills on 6/5/18.
+//  Copyright © 2018 Taylor Bills. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SinglePostTableViewCell : UITableViewCell
+
+@end
---
+++ WagCodeChallengeObj-C/View Controller/SinglePostTableViewCell.m
@@ -0,0 +1,15 @@
+//
+//  SinglePostTableViewCell.m
+//  WagCodeChallengeObj-C
+//
+//  Created by Taylor Bills on 6/5/18.
+//  Copyright © 2018 Taylor Bills. All rights reserved.
+//
+
+#import "SinglePostTableViewCell.h"
+
+@implementation SinglePostTableViewCell
+
+
+
+@end
